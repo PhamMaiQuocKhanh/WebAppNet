2c8d0f38306b22d9d7e4bd94bf6747a2ba626533
Admin <Admin@ADMIN-PC>
2024-09-09 16:19:44 +0700

    chỉnh sửa lại thư viện

---
+++ ThuVienTinhTienKhachSan/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ThuVienTinhTienKhachSan")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ThuVienTinhTienKhachSan")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4484100d-30b5-4295-9ba1-ad67cd5bf1ad")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ThuVienTinhTienKhachSan/TinhTienKhachSan.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThuVienTinhTienKhachSan
+{
+    public enum LoaiPhong
+    {
+        Vip,
+        Thuong
+    }
+    public class TinhTienKhachSan
+    {
+        private const int PhongVip = 300000;
+        private const int PhongThuong = 200000;
+
+        public static double TinhTongTien(LoaiPhong Phong, double Gio)
+        {
+            double giaTien = Phong == LoaiPhong.Vip ? PhongVip : PhongThuong;
+            return giaTien * Gio;
+        }
+    }
+    
+}
